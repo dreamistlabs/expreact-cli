3816334891bc77879bfd1be4e1190a6e1c6e1607
Johnny Hu <jgeehen.hu@gmail.com>
2017-08-08 16:46:42 -0700

    Add some shell commands to action

--- bin/expreact-cli.js
+++ bin/expreact-cli.js
@@ -7,7 +7,22 @@ program
   .arguments('<dir>')
   .usage('[options] <dir>')
   .action(function(dir) {
-    console.log(dir);
-    // shell.exec('create-react-app ' + dir);
+    console.log('express ' + dir);
+    // shell.exec('express ' + dir);
+
+    // simulate express folder creation
+    shell.mkdir(dir);
+    shell.cd(dir);
+    // simulate dependencies installation
+    // shell.exec('npm install');
+
+    // create random file
+    shell.touch('file.js');
+
+    // copy files over
+  })
+  .action(function(dir) {
+    console.log('create-react-app ' + dir + '/react-ui');
+    // shell.exec('create-react-app ' + dir + '/react-ui');
   })
   .parse(process.argv);
