166e96e0386f2a1a17dcb438e2c7e5c6cf29aeb6
Johnny Hu <jgeehen.hu@gmail.com>
2017-08-08 22:47:49 -0700

    Roughly map out the logic that modifies the express and react boilerplates so they interact with each other

--- bin/expreact-cli.js
+++ bin/expreact-cli.js
@@ -16,13 +16,52 @@ program
     // simulate dependencies installation
     // shell.exec('npm install');
 
-    // create random file
-    shell.touch('file.js');
+    // delete the default express public/ folder
+    shell.rm('-R', 'public');
+
+    // delete the default express views folder??
+    shell.rm('-R', 'views');
+
+    // update express package.json file to point express server to PORT 3001
+    shell.sed('-i',
+              '"start": "node ./bin/www"',
+              '"start": "PORT=3001 node ./bin/www"',
+              './package.json');
 
     // copy files over
+    // replace users.js with modified one to include sample data
+    shell.mkdir('routes');
+    shell.cp('-R', '../lib/express/*', './routes');
+
   })
   .action(function(dir) {
     console.log('create-react-app ' + dir + '/react-ui');
     // shell.exec('create-react-app ' + dir + '/react-ui');
+
+    // simulate react-ui creation
+    shell.mkdir('react-ui');
+    shell.mkdir('react-ui/src');
+    shell.touch('./react-ui/src/App.js');
+    shell.touch('./react-ui/src/App.css');
+    shell.touch('./react-ui/src/App.test.js');
+
+    // add proxy config property to react package.json file
+    shell.sed('-i',
+              /\s\s\}$/,
+              `  },\n  "proxy": "http://localhost:3001"`,
+              './react-ui/package.json');
+
+    // remove default App.js, App.css, and App.test.js files
+    shell.rm('-R', './react-ui/src/App.*');
+
+    // copy modified components folder over
+    shell.cp('-R', '../lib/react/components', './react-ui/src/');
+
+    // update index.js with new path to App.js
+    shell.sed('-i', "import App from './App';", "import App from './components/App';", './react-ui/src/index.js')
+
+    // TODO: RENAME APPLICATION BASED ON USER'S PROJECT NAME
+    // WHERE? React-UI package.json, React-UI public/index.html <title>
+
   })
   .parse(process.argv);
