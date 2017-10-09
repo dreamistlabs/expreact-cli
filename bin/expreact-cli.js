74759a4e0a84fefa04a7198510e3f43271b12950
Johnny Hu <heyitsjhu@gmail.com>
2017-10-09 14:12:27 -0700

    Add option to name react application folder name

--- bin/expreact-cli.js
+++ bin/expreact-cli.js
@@ -5,21 +5,25 @@ var child_process = require('child_process');
 
 program
   .version('0.0.1')
-  .arguments('<dir>')
+  .arguments('<dir> [reactDir]')
   .usage('[options] <dir>')
-  .action(function(dir) {
+  .action(function(dir, reactDir) {
     const location = shell.exec('which expreact', {silent: true})
                           .stdout
                           .trim()
                           .replace(/(\/\w+){2}$/, '');
 
     const PATH = location + '/lib/node_modules/expreact-cli';
+    const reactDirectory = reactDir ? reactDir : 'react-ui';
+    console.log(reactDirectory);
 
     // shell.exec('express ' + dir);
     // simulate express folder creation
     shell.mkdir(dir);
 
     shell.cd(dir);
+
+    shell.mkdir(['public', 'views']);
 
     // initialize a new npm package.json
     // child_process.execSync('npm init', {stdio: 'inherit'});
@@ -44,30 +48,30 @@ program
     shell.mkdir('routes');
     shell.cp('-R', PATH + '/lib/express/*', './routes');
 
-    console.log('create-react-app ' + dir + '/react-ui');
+    console.log('create-react-app ' + dir + '/' + reactDirectory);
     // shell.exec('create-react-app ' + dir + '/react-ui');
 
     // simulate react-ui creation
-    shell.mkdir('react-ui');
-    shell.mkdir('react-ui/src');
-    shell.touch('./react-ui/src/App.js');
-    shell.touch('./react-ui/src/App.css');
-    shell.touch('./react-ui/src/App.test.js');
+    shell.mkdir(reactDirectory);
+    shell.mkdir(reactDirectory + '/src');
+    shell.touch('./' + reactDirectory + '/src/App.js');
+    shell.touch('./' + reactDirectory + '/src/App.css');
+    shell.touch('./' + reactDirectory + '/src/App.test.js');
 
     // add proxy config property to react package.json file
-    shell.sed('-i',
-              /\s\s\}$/,
-              `  },\n  "proxy": "http://localhost:3001"`,
-              './react-ui/package.json');
+    // shell.sed('-i',
+    //           /\s\s\}$/,
+    //           `  },\n  "proxy": "http://localhost:3001"`,
+    //           './react-ui/package.json');
 
     // remove default App.js, App.css, and App.test.js files
-    shell.rm('-R', './react-ui/src/App.*');
+    shell.rm('-R', './' + reactDirectory + '/src/App.*');
 
     // copy modified components folder over
-    shell.cp('-R', PATH + '/lib/react/components', './react-ui/src/');
+    shell.cp('-R', PATH + '/lib/react/components', './' + reactDirectory + '/src/');
 
     // update index.js with new path to App.js
-    shell.sed('-i', "import App from './App';", "import App from './components/App';", './react-ui/src/index.js')
+    shell.sed('-i', "import App from './App';", "import App from './components/App';", './' + reactDirectory + '/src/index.js')
 
     // TODO: RENAME APPLICATION BASED ON USER'S PROJECT NAME
     // WHERE? React-UI package.json, React-UI public/index.html <title>
